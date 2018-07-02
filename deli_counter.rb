3caabcfcbc9a65744d74679f02b860bd875c506b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 21:37:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 katz_deli = []
 
-def take_a_number(katz_deli, name)
+def take_a_number(array, name)
   katz_deli<< name
   position = katz_deli.length 
   puts "Welcome, #{name}. You are number #{position} in line."
 end
 
 def line(katz_deli)
-  katz_deli.length==0? "The line is currently empty.": "boop"
+  katz_deli.length==0? "The line is currently empty.": katz_deli 
 end
 
 def now_serving
