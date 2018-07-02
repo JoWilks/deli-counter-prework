95d5070e1b80cda7869f2475c41d22ae8fd78e81
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 21:27:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,10 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-  katz_deli.length == 0 ? "The line is currently empty." : 
+  katz_deli.empty? ? "The line is currently empty." : 
     
 end
 
 def now_serving
-  katz_deli.empry?
+  katz_deli.empty?
 end
