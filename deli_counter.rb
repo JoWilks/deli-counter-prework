9b6ef58f39dfec7ca824abe673801eef25d212d4
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 21:33:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-  katz_deli.length == 0 ? puts "The line is currently empty.": puts "boop"
+  {katz_deli.length == 0 ? puts "The line is currently empty.": puts "boop"}
     
 end
 
