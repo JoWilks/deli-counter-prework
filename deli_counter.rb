e3ac6ff58a2cf4b472a1bac3ef3999c614281413
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 21:31:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def take_a_number(katz_deli, name)
   katz_deli<< name
   position = katz_deli.length 
-  puts "#{name}  #{position}"
+  puts "Welcome, #{name}. You are number #{position} in line."
 end
 
 def line(katz_deli)
